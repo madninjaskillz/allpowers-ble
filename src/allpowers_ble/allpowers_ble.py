fa72f46bcb08e696c897f7676cde5ba5fe8fd846
James Johnston <james@ezmuze.co.uk>
2023-10-07 18:09:32 +0100

    fix: linting

--- src/allpowers_ble/allpowers_ble.py
+++ src/allpowers_ble/allpowers_ble.py
@@ -16,7 +16,9 @@ from bleak_retry_connector import (
     establish_connection,
     retry_bluetooth_connection_error,
 )
+
 from .models import AllpowersState
+
 CHARACTERISTIC_NOTIFY = "0000FFF1-0000-1000-8000-00805F9B34FB"
 CHARACTERISTIC_WRITE = "0000FFF2-0000-1000-8000-00805F9B34FB"
 BLEAK_BACKOFF_TIME = 0.25
